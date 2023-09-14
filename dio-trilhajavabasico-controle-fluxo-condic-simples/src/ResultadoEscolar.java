7ef46cc7171ee2e2db06d85880582d3868b925e9
anacarladev <anacarlaoliveira.aco@gmail.com>
2023-09-14 16:33:54 -0300

    estrutura composta

---
+++ dio-trilhajavabasico-controle-fluxo-condic-simples/src/ResultadoEscolar.java
@@ -0,0 +1,12 @@
+public class ResultadoEscolar {
+    
+    public static void main(String[] args) {
+        int nota = 9;
+
+        if(nota >= 7)
+        System.out.println("aprovado");
+
+        else
+        System.out.println("reprovado");
+    }
+}
